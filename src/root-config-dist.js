afcd9fc33eb3abdee281fc0c4be450614a6f2f85
Joel Denning <joeldenning@gmail.com>
2019-06-12 15:11:27 +0300

    Hackathon stuff to get stuff working

--- src/root-config-dist.js
+++ src/root-config-dist.js
@@ -5,3 +5,35 @@ export * from "./root-config-lib";
 
 registerAllCoreApplications();
 start();
+
+// Workaround / waiting for https://github.com/systemjs/systemjs/issues/1939
+const moduleMap = {};
+
+export function getPublicPath(name) {
+  const path = moduleMap[name];
+  if (path) {
+    return path.slice(0, path.lastIndexOf("/") + 1);
+  } else {
+    throw Error("Cannot find public path for " + name);
+  }
+}
+
+const originalResolve = window.System.resolve;
+
+window.System.resolve = function(name) {
+  return originalResolve.apply(this, arguments).then(function(resolved) {
+    moduleMap[name] = resolved;
+    return resolved;
+  });
+};
+
+// We don't have a styleguide, so here's the very rudimentary version of one
+const css = `
+html {
+  background-color: #F4F5F8;
+}
+`;
+
+const styleEl = document.createElement("style");
+styleEl.textContent = css;
+document.head.appendChild(styleEl);
