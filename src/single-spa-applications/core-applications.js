c1daa777fa98118227021fc084a74086ad0d28da
Joel Denning <joeldenning@gmail.com>
2019-08-25 12:56:36 -0600

    Adding home app (#17)

--- src/single-spa-applications/core-applications.js
+++ src/single-spa-applications/core-applications.js
@@ -4,7 +4,8 @@ export const coreApplications = {
   "@openmrs/esm-login": shouldShowLogin,
   "@openmrs/esm-devtools": shouldShowDevtools,
   "@openmrs/esm-primary-navigation": shouldShowPrimaryNavigation,
-  "@openmrs/esm-patient-dashboard": shouldShowPatientDashboard
+  "@openmrs/esm-patient-dashboard": shouldShowPatientDashboard,
+  "@openmrs/esm-home": shouldShowHome
 };
 
 // To learn more about top level routing in single-spa, go to https://single-spa.js.org/docs/configuration.html#activity-function
@@ -23,4 +24,8 @@ function shouldShowPrimaryNavigation(location) {
 
 function shouldShowPatientDashboard(location) {
   return routeRegex(/^patient\/.+\/dashboard/, location);
+}
+
+function shouldShowHome(location) {
+  return routePrefix("home", location);
 }
