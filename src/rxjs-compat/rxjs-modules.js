e609b7cc3b785a42409653da2ff0e4b06c208a4d
Joel Denning <joeldenning@gmail.com>
2019-09-06 20:51:48 -0600

    Self review

--- src/rxjs-compat/rxjs-modules.js
+++ src/rxjs-compat/rxjs-modules.js
@@ -4,7 +4,7 @@
  * the `/` character as a directory and expect there to be an entirely
  * separate file for the rxjs operators. The rxjs lib itself puts the operators
  * into the same umd file as the `rxjs` core stuff, not into a separate file.
- * This file makes it so that `import 'rxjs/operators'` works without requiring
+ * The code here makes it so that `import 'rxjs/operators'` works without requiring
  * it to be in a separate file.
  */
 
