f9d942cbc2da12422aa7c6d24cfb5fd8b4550d39
Brandon Istenes <bistenes@gmail.com>
2020-05-05 16:20:12 -0700

    Add mutation observer to watch for language change

--- src/setup-tests.js
+++ src/setup-tests.js
@@ -2,3 +2,9 @@ window.System = {
   resolve: jest.fn(),
   register: jest.fn()
 };
+
+global.MutationObserver = class {
+  constructor(callback) {}
+  disconnect() {}
+  observe(element, initObject) {}
+};
